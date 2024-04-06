aa63c65588912f38cd8dff12768bf2e75bf4af1e
Leong Zhe Cheng <zhecheng555@gmail.com>
2024-04-06 16:26:53 +0800

    changed end game screen

--- app/src/main/java/com/example/cs206/EndGameActivity.java
+++ app/src/main/java/com/example/cs206/EndGameActivity.java
@@ -11,22 +11,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EndGameActivity extends AppCompatActivity {
 
-    private ImageView backgroundImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
-
-        // Reference to the background ImageView
-        backgroundImageView = (ImageView) findViewById(R.id.backgroundImageView);
-
-        // Set the scaled bitmap as background
-        setBackground();
+        EdgeToEdge.enable(this);
 
         Button restartButton = (Button) findViewById(R.id.restartButton);
         Button menuButton = (Button) findViewById(R.id.menuButton);
@@ -56,36 +51,36 @@ public class EndGameActivity extends AppCompatActivity {
         });
 
         // Calculate the top margin to position the button at the third quarter of the screen height
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int screenHeight = displayMetrics.heightPixels;
-        int topMargin = (int) (screenHeight * 0.7);
-        int menuTopMargin = (int) (screenHeight * 0.8);;
-
-        // Set the parameters for the button
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) restartButton.getLayoutParams();
-        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        layoutParams.topMargin = topMargin;
-        restartButton.setLayoutParams(layoutParams);
-
-        // Set the parameters for the button
-        RelativeLayout.LayoutParams menuLayoutParams = (RelativeLayout.LayoutParams) menuButton.getLayoutParams();
-        menuLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        menuLayoutParams.topMargin = menuTopMargin;
-        menuButton.setLayoutParams(menuLayoutParams);
+//        DisplayMetrics displayMetrics = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+//        int screenHeight = displayMetrics.heightPixels;
+//        int topMargin = (int) (screenHeight * 0.7);
+//        int menuTopMargin = (int) (screenHeight * 0.8);;
+//
+//        // Set the parameters for the button
+//        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) restartButton.getLayoutParams();
+//        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+//        layoutParams.topMargin = topMargin;
+//        restartButton.setLayoutParams(layoutParams);
+//
+//        // Set the parameters for the button
+//        RelativeLayout.LayoutParams menuLayoutParams = (RelativeLayout.LayoutParams) menuButton.getLayoutParams();
+//        menuLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+//        menuLayoutParams.topMargin = menuTopMargin;
+//        menuButton.setLayoutParams(menuLayoutParams);
     }
 
-    private void setBackground() {
-        // Get the size of the screen
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
-        int screenWidth = metrics.widthPixels;
-        int screenHeight = metrics.heightPixels;
-
-        // Load and scale the background to fit the screen
-        Bitmap originalBackground = BitmapFactory.decodeResource(getResources(), R.drawable.sg_3);
-        Bitmap background = Bitmap.createScaledBitmap(originalBackground, screenWidth, screenHeight, false);
-
-        // Set the scaled bitmap as the source for the ImageView
-        backgroundImageView.setImageBitmap(background);
-    }
+//    private void setBackground() {
+//        // Get the size of the screen
+//        DisplayMetrics metrics = getResources().getDisplayMetrics();
+//        int screenWidth = metrics.widthPixels;
+//        int screenHeight = metrics.heightPixels;
+//
+//        // Load and scale the background to fit the screen
+//        Bitmap originalBackground = BitmapFactory.decodeResource(getResources(), R.drawable.sg_3);
+//        Bitmap background = Bitmap.createScaledBitmap(originalBackground, screenWidth, screenHeight, false);
+//
+//        // Set the scaled bitmap as the source for the ImageView
+//        backgroundImageView.setImageBitmap(background);
+//    }
 }
